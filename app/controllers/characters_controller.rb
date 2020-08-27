bcc4935db9450506ff6a894d645d4cf8d19d146f
Directrix777 <andrew.j.ribas@gmail.com>
2020-08-27 14:13:04 -0500

    Added routing to Characters controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,8 +1,76 @@
 class CharactersController < ApplicationController
 
     get '/characters' do
+        if !Helpers.is_logged_in?(session)
+            redirect to '/login'
+        end
         @characters = Character.all
-        erb :index
+        erb :'characters/index'
+    end
+
+    get '/characters/new' do
+        if !Helpers.is_logged_in?(session)
+            redirect to '/login'
+        end
+        erb :'characters/new'
+    end
+
+    post '/characters' do
+        if params.values.include?("")
+            session[:error] = "Please fill in all fields"
+            redirect '/characters/new'
+        end
+        character = Character.create(params[:character])
+        character.user_id = session[:user_id]
+        character.save
+
+        redirect '/characters'
+    end
+
+    get '/characters/:id' do
+        if !Helpers.is_logged_in?(session)
+            redirect to '/login'
+        end
+        @character = Character.find(params[:id])
+        erb :'characters/show'
+    end
+
+    get '/characters/:id/edit' do
+        if !Helpers.is_logged_in?(session)
+            redirect to '/login'
+        end
+        @character = Character.find(params[:id])
+        erb :'characters/edit'
+    end
+
+    patch '/characters/:id' do
+        character = Character.find(params[:id])
+        if params[:character][:content] == ""
+            session[:error] = "Character cannot be blank"
+            redirect "/characters/#{character.id}/edit"
+        end
+        if !Helpers.is_logged_in?(session)
+            redirect to '/login'
+        end
+        user = User.find(session[:user_id])
+        if user == character.user
+            character.update(params[:character])
+
+        end
+    end
+
+    delete '/characters/:id' do
+        if !Helpers.is_logged_in?(session)
+            redirect to '/login'
+        end
+        user = User.find(session[:user_id])
+        character = Character.find(params[:id])
+        if user != character.user
+            redirect "/characters/#{character.id}"
+        end
+        Character.destroy(params[:id])
+        redirect '/characters'
+        
     end
 
 
