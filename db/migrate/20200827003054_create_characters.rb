0e13125dd63ccc69d9fb9808d8218aa2f637e45c
Directrix777 <andrew.j.ribas@gmail.com>
2020-08-26 20:34:33 -0500

    wrote out migrations

--- db/migrate/20200827003054_create_characters.rb
+++ db/migrate/20200827003054_create_characters.rb
@@ -1,4 +1,25 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :barbarian_level
+      t.integer :bard_level
+      t.integer :cleric_level
+      t.integer :druid_level
+      t.integer :fighter_level
+      t.integer :monk_level
+      t.integer :paladin_level
+      t.integer :ranger_level
+      t.integer :rogue_level
+      t.integer :sorcerer_level
+      t.integer :warlock_level
+      t.integer :wizard_level
+      t.integer :str
+      t.integer :dex
+      t.integer :con
+      t.integer :int
+      t.integer :wis
+      t.integer :cha
+    end
   end
 end
