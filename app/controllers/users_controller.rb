e8c84ed68ffa5096ae1b86241e367d498cf40c3b
Directrix777 <andrew.j.ribas@gmail.com>
2020-08-27 10:52:16 -0500

    added user routing as well as slug functionality for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+    get '/signup' do
+        if Helpers.is_logged_in?(session)
+            redirect to '/characters'
+        end
+        erb :'users/new'
+    end
+
+    post '/signup' do
+        user = User.new(params)
+        if !(user.save)
+            session[:error] = "Please fill out all fields"
+            redirect '/signup'
+        end
+        session[:user_id] = user.id
+        redirect to '/characters'
+    end
+
+    get '/logout' do
+        if !Helpers.is_logged_in?(session)
+            redirect to '/characters'
+        end
+        session.clear
+        redirect '/login'
+    end
+
+    get '/login' do
+        if Helpers.is_logged_in?(session)
+            redirect to '/characters'
+        end
+        erb :'users/login'
+    end
+
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/characters"
+        else
+            session[:error] = "Incorrect username or password"
+			redirect "/login"
+		end
+    end
+
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/show'
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :characters
     validates_presence_of :username, :email, :password_digest
+
+    def slug
+        self.username.split(" ").join("-")
+    end
+    
+    def self.find_by_slug(slug)
+        self.find_by_username(slug.split("-").join(" "))
+    end
 end
