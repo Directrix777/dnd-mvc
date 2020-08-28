508bb5f7641d76cb44581005aaf05fe354250e8e
Directrix777 <andrew.j.ribas@gmail.com>
2020-08-28 13:12:55 -0500

    get_pretty_levels is now more efficient also sorts existing classes by level in descending order

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -10,11 +10,9 @@ class Helpers
 
     def self.get_pretty_levels(character)
         level_text = []
-        levels = {"Barbarian" => character.barbarian_level, "Bard" => character.bard_level, "Cleric" => character.cleric_level, "Druid" => character.druid_level, "Fighter" => character.fighter_level, "Monk" => character.monk_level, "Paladin" => character.paladin_level, "Ranger" => character.ranger_level, "Rogue" => character.rogue_level, "Sorcerer" => character.sorcerer_level, "Warlock" => character.warlock_level, "Wizard" => character.wizard_level}
+        levels = {"Barbarian" => character.barbarian_level, "Bard" => character.bard_level, "Cleric" => character.cleric_level, "Druid" => character.druid_level, "Fighter" => character.fighter_level, "Monk" => character.monk_level, "Paladin" => character.paladin_level, "Ranger" => character.ranger_level, "Rogue" => character.rogue_level, "Sorcerer" => character.sorcerer_level, "Warlock" => character.warlock_level, "Wizard" => character.wizard_level}.select{|k, v| v > 0}.sort_by{|k,v| -v}.to_h
         levels.each do |closs, level| #closs is used to avoid "class" keyword
-            if level > 0
                 level_text << "Level #{level}  #{closs}"
-            end
         end
         level_text
     end
