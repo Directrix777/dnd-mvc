73303543398135fbd006ae9e4532c0c7b1396b77
Directrix777 <andrew.j.ribas@gmail.com>
2020-08-28 11:30:36 -0500

    Fixed issue with method naming get_pretty levels changed to class method

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,14 +1,14 @@
 class Helpers
 
-    def character.current_user(session)
+    def self.current_user(session)
         User.find_by(id: session[:user_id])
     end
 
-    def character.is_logged_in?(session)
+    def self.is_logged_in?(session)
         !!session[:user_id]
     end
 
-    def get_pretty_levels(character)
+    def self.get_pretty_levels(character)
         level_text = []
         levels = {"Barbarian" => character.barbarian_level, "Bard" => character.bard_level, "Cleric" => character.cleric_level, "Druid" => character.druid_level, "Fighter" => character.fighter_level, "Monk" => character.monk_level, "Paladin" => character.paladin_level, "Ranger" => character.ranger_level, "Rogue" => character.rogue_level, "Sorcerer" => character.sorcerer_level, "Warlock" => character.warlock_level, "Wizard" => character.wizard_level}
         levels.each do |closs, level| #closs is used to avoid "class" keyword
